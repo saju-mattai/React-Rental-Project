693857c6daf30bd4cc24a1c08705f9c657a433ed
saju-mattai <msaju682@gmail.com>
2023-06-22 17:26:51 +0530

    bug fixed

--- client/src/API/User/ApiCalls.js
+++ client/src/API/User/ApiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "https://bikerental.store/api/user" });
-const API = axios.create({ baseURL: "http://localhost:8080/api/user" })
+const API = axios.create({ baseURL: "https://bikerental.store/api/user" });
+// const API = axios.create({ baseURL: "http://localhost:8080/api/user" })
 
 
 const user = JSON.parse(localStorage.getItem("UserInfo"));
