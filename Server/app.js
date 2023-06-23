57604cf75b3413ea0bcfa937ce96ea74fc12ff42
saju-mattai <msaju682@gmail.com>
2023-06-23 10:02:35 +0530

    updated cors

--- Server/app.js
+++ Server/app.js
@@ -29,8 +29,8 @@ var app = express();
 
 //cors
 const corsOptions = {
-  // origin: "https://rent-a-drive.netlify.app",
-  origin: "*",
+  origin: ["https://rent-a-drive.netlify.app","http://rent-a-drive.netlify.app","*"],
+  // origin: "*",
   credentials: true,
   optioSuccessStatus: 200,
 };
